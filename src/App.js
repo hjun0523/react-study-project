b0dce4d259e6f54f67c9e156b8d086292cfa05ea
hjun0523 <rla343477@gmail.com>
2021-08-29 22:56:33 +0900

    commit test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import './App.css';
 
-function App() {
+function App() {  
   // 요청받은 정보를 담아줄 변수 선언
   const [ testStr, setTestStr ] = useState('');
 
   // 변수 초기화
   function callback(str) {
     setTestStr(str);
-  }  
+  }    
 
   // 첫 번째 렌더링을 마친 후 실행
   useEffect(
